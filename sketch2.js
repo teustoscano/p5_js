95fdc756ea0610eccc6297803086782cc6a44781
Teus Toscano <ssstoscano@gmail.com>
2016-10-25 18:30:33 -0200

    Using if statement to make appears another two circles

--- sketch2.js
+++ sketch2.js
@@ -8,6 +8,11 @@ function draw(){
 	stroke(255);
 	strokeWeight(4);
 	noFill();
+	if (mouseX > width/2){
+		fill(255, 0, 200);
+		ellipse(100, 200, 40, 40);
+		ellipse(500, 200, 40, 40);
+	}
 	ellipse(300, 200, 100, 100);
 }
 
