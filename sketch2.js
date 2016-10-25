80460089d27284dbc47aeecbc70e1ebe9a34c16b
Teus Toscano <ssstoscano@gmail.com>
2016-10-25 18:21:46 -0200

    Basic creation of sketch2.js for some new stuff

---
+++ sketch2.js
@@ -0,0 +1,15 @@
+function setup(){
+	createCanvas(640, 360);
+}
+
+function draw(){
+	background(0);
+
+	stroke(255);
+	strokeWeight(4);
+	noFill();
+	ellipse(300, 200, 100, 100);
+}
+
+function mousePressed(){
+}
