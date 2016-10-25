0128bbfdc8ecdbf20d444864b40bfd0fc0c00632
Teus Toscano <ssstoscano@gmail.com>
2016-10-25 15:22:05 -0200

    Creating the idea of an paint brush like

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 function setup () {
 	createCanvas(640, 360);
+	//background
+	background(250, 250, 100);
 }
 
 function draw() {
-	//background
-	background(250, 250, 100);
 
 	//ellipse
+	noStroke();
 	fill(250, 200, 200);
-	ellipse(100, 100, 100, 100);
+	ellipse(mouseX, mouseY, 40, 40);
 
 	//rectangle
 	fill(200, 250, 200);
