52aa23d0f17b0802f1f8cd4911ad39a6fcf1b780
Teus Toscano <ssstoscano@gmail.com>
2016-10-25 15:53:35 -0200

    Moving a ball side-to-side

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
-function setup () {
+var circleX = 0;
+
+function setup(){
 	createCanvas(640, 360);
-	//background
-	background(250, 250, 100);
+	
 }
 
-function draw() {
+function draw(){
+	//background
+	background(250, 250, 100);
 
 	//ellipse
 	noStroke();
 	fill(250, 200, 200);
-	ellipse(mouseX, mouseY, 40, 40);
+	ellipse(circleX+=5, 100, 40, 40);
+}
 
-	//rectangle
-	fill(200, 250, 200);
-	rect(400, 100, 50, 50);
+function mousePressed(){
+	//background
+	background(250, 250, 100);
 }
