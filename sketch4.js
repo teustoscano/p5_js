921870404e960878cad3a9afdd59b59c35ede277
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 12:29:09 -0200

    setting up the sketch4.js

---
+++ sketch4.js
@@ -0,0 +1,16 @@
+function setup(){
+	createCanvas(640, 360);
+}
+
+function draw(){
+	background(0);
+	strokeWeight(4);
+	stroke(255);
+
+	ellipse(0, 200, 25, 25);
+	ellipse(50, 200, 25, 25);
+	ellipse(100, 200, 25, 25);
+	ellipse(150, 200, 25, 25);
+	ellipse(200, 200, 25, 25);
+	ellipse(250, 200, 25, 25);
+}
