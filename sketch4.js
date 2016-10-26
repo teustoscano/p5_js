c340698dd7c4c0bc7f2315d00b2b145ffd92abac
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 12:49:24 -0200

    Making a line of blinked circles go offset the screen

--- sketch4.js
+++ sketch4.js
@@ -1,21 +1,17 @@
+var offset = 0;
+
 function setup(){
-	createCanvas(600, 360);
+	createCanvas(600, 400);
 }
 
 function draw(){
 	background(0);
 	strokeWeight(4);
 	stroke(255);
-	var x = 0;
-	while (x <= width) {
-		fill(200,200,0);
-		ellipse(x, 100, 25, 25);	
-		x += 50;
-	}
+
 	for(var x = 0; x <= width; x+=50){
-		fill(200,100,100);
-		ellipse(x, 300, 25, 25);	
-		x += 50;
+		fill(random(255), 0, random(255));
+		ellipse(x+offset, 200, 25, 25);	
 	}
-	
+	offset += 1;
 }
