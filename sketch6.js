9195e0851fd76db6173d919e40c382f7e8765851
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 14:36:27 -0200

    Array of bubbles

--- sketch6.js
+++ sketch6.js
@@ -1,25 +1,30 @@
-var bubble ={
-	x: 300,
-	y: 200,
-
-	display: function(){
-		stroke(255);
-		strokeWeight(4);
-		noFill();
-		ellipse(this.x, this.y, 24, 24);	
-	},
-	move: function(){
-		this.x = this.x + random(-1,1);
-		this.y = this.y + random(-1,1);
-	}
-}
+var bubbles = [];
 
 function setup(){
 	createCanvas(600, 400);
+	for(var i = 0; i < 15;i++){
+		bubbles[i] ={
+			x: 300,
+			y: 200,
+
+			display: function(){
+				stroke(255);
+				strokeWeight(2 );
+				noFill();
+				ellipse(this.x, this.y, 24, 24);	
+			},
+			move: function(){
+				this.x = this.x + random(-1,1);
+				this.y = this.y + random(-1,1);
+			}
+		}
+	}
 }
 
 function draw(){
 	background(0);
-	bubble.move();
-	bubble.display();
+	for(var i = 0; i < bubbles.length; i++){
+		bubbles[i].move();
+		bubbles[i].display();
+	}
 }
