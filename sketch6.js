570db1b291874e3d5cce2270e1d5be58f0e4b2dc
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 14:49:06 -0200

    Array of bubbles made by a constructor

--- sketch6.js
+++ sketch6.js
@@ -3,21 +3,7 @@ var bubbles = [];
 function setup(){
 	createCanvas(600, 400);
 	for(var i = 0; i < 15;i++){
-		bubbles[i] ={
-			x: 300,
-			y: 200,
-
-			display: function(){
-				stroke(255);
-				strokeWeight(2 );
-				noFill();
-				ellipse(this.x, this.y, 24, 24);	
-			},
-			move: function(){
-				this.x = this.x + random(-1,1);
-				this.y = this.y + random(-1,1);
-			}
-		}
+		bubbles[i] = new Bubble();
 	}
 }
 
@@ -26,5 +12,21 @@ function draw(){
 	for(var i = 0; i < bubbles.length; i++){
 		bubbles[i].move();
 		bubbles[i].display();
+	}
+}
+
+function Bubble() {
+	this.x = 300,
+	this.y = 200,
+
+	this.display = function(){
+		stroke(255);
+		strokeWeight(2 );
+		noFill();
+		ellipse(this.x, this.y, 24, 24);	
+	},
+	this.move = function(){
+		this.x = this.x + random(-1,1);
+		this.y = this.y + random(-1,1);
 	}
 }
