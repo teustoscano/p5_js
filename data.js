5213f1123a58d9e1c46339e6cd0eed7549b776f4
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 19:56:47 -0200

    Using a JSON file and loading it on my js

--- data.js
+++ data.js
@@ -1,5 +1,16 @@
+var flower;
+
+function preload(){
+	flower = loadJSON("flower.json");
+}
+
 function setup(){
+	createCanvas(600, 300);
 }
 
 function draw(){
+	background(0);
+
+	fill(flower.r, flower.g, flower.b);
+	text(flower.name, 10, 50);
 }
