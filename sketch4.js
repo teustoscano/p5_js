9a8f104320eb3857ea254197116a33612c4a3d0e
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 12:59:42 -0200

    Circles blinking along the mouseY moviment

--- sketch4.js
+++ sketch4.js
@@ -8,8 +8,9 @@ function draw(){
 	stroke(255);
 
 	for(var x = 0; x <= width; x+=50){
-		for(var y = 0; y <= height; y+=50){
-			fill(random(255), 0, random(255));
+		fill(random(255), 100, random(255));
+		for(var y = 0; y <= mouseY; y+=50){
+			//fill(random(255), 0, random(255));
 			ellipse(x, y, 25, 25);
 		}	
 	}
