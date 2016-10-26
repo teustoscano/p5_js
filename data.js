cc88bbeffe423b0ee521100f039c852281716865
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 20:51:39 -0200

    Using a JSON url to display the numbers of astronauts in the space

--- data.js
+++ data.js
@@ -1,16 +1,18 @@
-var flower;
-
-function preload(){
-	flower = loadJSON("flower.json");
-}
-
 function setup(){
 	createCanvas(600, 300);
+	loadJSON("http://api.open-notify.org/astros.json", gotData, "jsonp");
+}
+
+function gotData(data) {
+	background(0);
+	for(var i = 0; i < data.number; i++){
+		if (data.people[i].craft == "ISS") {
+			fill(255, 0, 50);
+		}else
+		fill(255);
+		ellipse(random(width), random(height), 50, 50);
+	}
 }
 
 function draw(){
-	background(0);
-
-	fill(flower.r, flower.g, flower.b);
-	text(flower.name, 10, 50);
 }
