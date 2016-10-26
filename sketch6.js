af049a05fffc0b39c59e35a5fc38107a28fae590
Teus Toscano <ssstoscano@gmail.com>
2016-10-26 13:56:44 -0200

    Bubble object

--- sketch6.js
+++ sketch6.js
@@ -1,6 +1,17 @@
 var bubble ={
 	x: 300,
 	y: 200,
+
+	display: function(){
+		stroke(255);
+		strokeWeight(4);
+		noFill();
+		ellipse(this.x, this.y, 24, 24);	
+	},
+	move: function(){
+		this.x = this.x + random(-1,1);
+		this.y = this.y + random(-1,1);
+	}
 }
 
 function setup(){
@@ -9,18 +20,6 @@ function setup(){
 
 function draw(){
 	background(0);
-	move();
-	display();
-}
-
-function move(){
-	bubble.x = bubble.x + random(-1,1);
-	bubble.y = bubble.y + random(-1,1);	
-}
-
-function display(){
-	stroke(255);
-	strokeWeight(4);
-	noFill();
-	ellipse(bubble.x, bubble.y, 24, 24);
+	bubble.move();
+	bubble.display();
 }
