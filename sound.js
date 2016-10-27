bec7e7b5424b3b6af9b2b969f98d85fb84e7861d
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 15:20:27 -0200

    Playing my jam nigga, a nice beat in the browser man

--- sound.js
+++ sound.js
@@ -1,5 +1,14 @@
+var song;
+
+function preload() {
+	song = loadSound("Birds_Of_Prey.ogg");
+}
+
 function setup() {
+	createCanvas(300, 200);
+	song.play();
 }
 
 function draw() { 
+	background(0);
 }
