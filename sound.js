ff96584a823f809fca3156c0655f4436c9cd87db
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 15:41:02 -0200

    Adding sliders for the volume, rate and the pan

--- sound.js
+++ sound.js
@@ -1,14 +1,24 @@
 var song;
-
-function preload() {
-	song = loadSound("Birds_Of_Prey.ogg");
-}
+var sliderVolume;
+var sliderPan;
+var sliderRate;
 
 function setup() {
 	createCanvas(300, 200);
+	song = loadSound("Birds_Of_Prey.ogg", loaded);
+	sliderVolume = createSlider(0, 1, 0.5, 0.01);
+	sliderRate = createSlider(0, 2, 1, 0.01);
+	sliderPan = createSlider(-1, 1, 0, 0.01);//side ear to another ear
+	
+}
+
+function loaded(){
 	song.play();
 }
 
 function draw() { 
 	background(0);
+	song.setVolume(sliderVolume.value());
+	song.pan(sliderPan.value());
+	song.rate(sliderRate.value());
 }
