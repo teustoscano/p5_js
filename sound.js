37e3a13f2d0f80ea50addcf14eeda1cefcb6426c
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 15:54:32 -0200

    Adding a play/pause button

--- sound.js
+++ sound.js
@@ -2,18 +2,31 @@ var song;
 var sliderVolume;
 var sliderPan;
 var sliderRate;
+var button;
 
 function setup() {
 	createCanvas(300, 200);
 	song = loadSound("Birds_Of_Prey.ogg", loaded);
+	button = createButton("i>");
+	button.mousePressed(tooglePlaying);
 	sliderVolume = createSlider(0, 1, 0.5, 0.01);
 	sliderRate = createSlider(0, 2, 1, 0.01);
 	sliderPan = createSlider(-1, 1, 0, 0.01);//side ear to another ear
 	
 }
 
+function tooglePlaying () {
+	if (!song.isPlaying()){
+		song.play();
+		button.html("ii");		
+	}else{
+		song.pause();
+		button.html("i>");
+	}
+}
+
 function loaded(){
-	song.play();
+	//song.play();
 }
 
 function draw() { 
