4a86226cf552d8b553b62882fa6f7fe27de2f9b5
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 16:27:57 -0200

    Adding a jump button, and use currentTime and Cue

--- sound.js
+++ sound.js
@@ -3,16 +3,28 @@ var sliderVolume;
 var sliderPan;
 var sliderRate;
 var button;
+var jumpButton;
 
 function setup() {
 	createCanvas(300, 200);
 	song = loadSound("Birds_Of_Prey.ogg", loaded);
 	button = createButton("i>");
 	button.mousePressed(tooglePlaying);
-	sliderVolume = createSlider(0, 1, 0.5, 0.01);
-	sliderRate = createSlider(0, 2, 1, 0.01);
-	sliderPan = createSlider(-1, 1, 0, 0.01);//side ear to another ear
-	
+	//jumpButton = createButton("Jump");
+	//jumpButton.mousePressed(jumpSong);
+	//sliderVolume = createSlider(0, 1, 0.5, 0.01);
+	//sliderRate = createSlider(0, 2, 1, 0.01);
+	//sliderPan = createSlider(-1, 1, 0, 0.01);//side ear to another ear
+	//song.addCue(5, changeBackground);
+}
+
+function changeBackground(){
+	background(random(255), random(255), random(255));
+}
+
+function jumpSong() {
+	var len = song.duration();
+	song.jump(len/2);
 }
 
 function tooglePlaying () {
@@ -30,8 +42,11 @@ function loaded(){
 }
 
 function draw() { 
-	background(0);
-	song.setVolume(sliderVolume.value());
-	song.pan(sliderPan.value());
-	song.rate(sliderRate.value());
+	//background(0);
+	//song.setVolume(sliderVolume.value());
+	//song.pan(sliderPan.value());
+	//song.rate(sliderRate.value());
+	//if (song.currentTime() > 5) {
+		background(song.currentTime()*5, 100, 100);
+	//}
 }
