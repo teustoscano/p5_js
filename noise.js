3dbc504b2e5423c7bbf4e78c2088bdcea4256297
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 21:37:55 -0200

    Tv effect

--- noise.js
+++ noise.js
@@ -1,28 +1,26 @@
 //var xoff1 = 0;
 //var xoff2 = 100;
 
+var inc = 0.01;
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(200, 200);
 }
 
 function draw() {
-	background(0);
-	
-	//var x = map(noise(xoff1), 0, 1, 0, width);
-	//var y = map(noise(xoff2), 0, 1, 0, height);
-	
-	//xoff1 += 0.01;
-	//xoff2 += 0.02;
-	beginShape();
-	var xoff = 0;
-	for(var x = 0; x < width; x++){
-		stroke(255);
-		var y = noise(xoff)*height;
-		vertex(x, y);
+	loadPixels();
 
-		xoff += 0.01;
+	for(var x = 0; x < width; x++){
+		for(var y = 0; y < height; y++){
+			var index = (x + y * width) * 4;
+			var r = random(255);
+			pixels[index+0] = r;
+			pixels[index+1] = r;
+			pixels[index+2] = r;
+			pixels[index+3] = r;
+		}
 	}
-	endShape();
+	updatePixels();
 
 	//stroke(255);
 	//ellipse(x, y, 20, 20);
