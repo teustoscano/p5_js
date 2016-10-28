acee995b59624bfb61412434b3016eff1bdca95e
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 22:03:20 -0200

    Oscillator with a slider for the frequency

--- sound2.js
+++ sound2.js
@@ -1,16 +1,19 @@
 var wave;
+var sliderFreq;
 
 function setup(){
 	createCanvas(300, 300);
 
 	wave = new p5.Oscillator();
+	sliderFreq = createSlider(100, 1200, 0, 0.01);
 	wave.setType("sine");
 	wave.start();
 	wave.amp(1);
-	wave.freq(520);
+	
 	
 }
 
 function draw(){
 	background(0);
+	wave.freq(sliderFreq.value());
 }
