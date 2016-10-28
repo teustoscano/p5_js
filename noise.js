c68c068a5634065fb9d1ea9c7a734f900359eece
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 21:25:34 -0200

    Graphic representation one dimensio perlin noise

--- noise.js
+++ noise.js
@@ -1,4 +1,5 @@
-var xoff = 0;
+//var xoff1 = 0;
+//var xoff2 = 100;
 
 function setup() {
 	createCanvas(400, 400);
@@ -7,9 +8,22 @@ function setup() {
 function draw() {
 	background(0);
 	
-	var x = map(noise(xoff), 0, 1, 0, width);
-	xoff += 0.01;
+	//var x = map(noise(xoff1), 0, 1, 0, width);
+	//var y = map(noise(xoff2), 0, 1, 0, height);
+	
+	//xoff1 += 0.01;
+	//xoff2 += 0.02;
+	beginShape();
+	var xoff = 0;
+	for(var x = 0; x < width; x++){
+		stroke(255);
+		var y = noise(xoff)*height;
+		vertex(x, y);
 
-	stroke(255);
-	ellipse(x, 200, 20, 20);
+		xoff += 0.01;
+	}
+	endShape();
+
+	//stroke(255);
+	//ellipse(x, y, 20, 20);
 }
