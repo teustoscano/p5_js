1b8dc091e74f3bd80c29fb9c08e444bde47996dd
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 21:50:57 -0200

    Blur, perlin noise

--- noise.js
+++ noise.js
@@ -8,17 +8,24 @@ function setup() {
 }
 
 function draw() {
+	
+	var yoff = 0;
+
 	loadPixels();
 
 	for(var x = 0; x < width; x++){
+		var xoff = 0;
 		for(var y = 0; y < height; y++){
 			var index = (x + y * width) * 4;
-			var r = random(255);
+			var r = noise(xoff, yoff) * 255;
 			pixels[index+0] = r;
 			pixels[index+1] = r;
 			pixels[index+2] = r;
 			pixels[index+3] = r;
+
+			xoff += 0.01;
 		}
+		yoff += 0.01;
 	}
 	updatePixels();
 
