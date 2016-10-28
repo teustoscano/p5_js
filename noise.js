f7bb5c04b977ddd3edfa588dd91e05ffac20b034
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 21:05:12 -0200

    Ball moving in the x axis with noise perlin

--- noise.js
+++ noise.js
@@ -1,8 +1,15 @@
+var xoff = 0;
+
 function setup() {
 	createCanvas(400, 400);
 }
 
 function draw() {
 	background(0);
-	ellipse(200, 200, 20, 20);
+	
+	var x = map(noise(xoff), 0, 1, 0, width);
+	xoff += 0.01;
+
+	stroke(255);
+	ellipse(x, 200, 20, 20);
 }
