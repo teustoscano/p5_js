43088e7f437f03d406e16038dd44f3aaa709a30f
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 17:48:08 -0200

    Using sockets.io in the client code

--- public/web.js
+++ public/web.js
@@ -1,8 +1,14 @@
+var socket;
+
 function setup(){
 	createCanvas(300, 400);
+	background(0);
+
+	socket = io.connect('http://localhost:3000');
 }
 
-function draw(){
-	background(0);
+function draw(){	
+	noStroke();
+	fill(255); 
 	ellipse(mouseX, mouseY, 20, 20);
 }
