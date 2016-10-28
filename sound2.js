89e78a1206f0f91e22e4c0b85b59439e92a57a0e
Teus Toscano <ssstoscano@gmail.com>
2016-10-27 22:23:41 -0200

    Envelope ADSR

--- sound2.js
+++ sound2.js
@@ -1,19 +1,25 @@
 var wave;
 var sliderFreq;
+var env;
 
 function setup(){
 	createCanvas(300, 300);
 
+	env = new p5.Env();
+	env.setADSR(0.05, 0.1, 0.5, 0.2);
+	env.setRange(0.8, 0) ;
+
 	wave = new p5.Oscillator();
-	sliderFreq = createSlider(100, 1200, 0, 0.01);
 	wave.setType("sine");
 	wave.start();
-	wave.amp(1);
-	
+	wave.amp(env);
+	wave.freq(400);
+	sliderFreq = createSlider(100, 1200, 0, 0.01);
+	env.play();
 	
 }
 
 function draw(){
 	background(0);
-	wave.freq(sliderFreq.value());
+	//sliderFreq.value()
 }
