7877d9011aef077e71b306290173161ea49edfcb
Teus Toscano <ssstoscano@gmail.com>
2016-10-28 22:17:35 -0200

    Three dimensional Perlin noise in a two dimensional visualization of arrows

--- noise.js
+++ noise.js
@@ -2,27 +2,41 @@
 //var xoff2 = 100;
 
 var inc = 0.01;
-var scl = 20;
+var scl = 10;
 var cols;
 var rows;
+var zoff = 0;
+var fr;
 
 function setup() {
 	createCanvas(200, 200);
 	cols = floor(width/scl);
 	rows = floor(height/scl);
+	fr = createP('');
 }
 
 function draw() {
+	background(255);
 	var yoff = 0;
+
 	for(var x = 0; x < cols; x++){
 		var xoff = 0;
 		for(var y = 0; y < rows; y++){
 			var index = (x + y * width) * 4;
-			var r = noise(xoff, yoff) * 255;
+			var angle = noise(xoff, yoff, zoff) * TWO_PI;
+			var v = p5.Vector.fromAngle(angle);
 			xoff += 0.01;
-			fill(random(255));
-			rect(x*scl, y*scl, scl, scl);
+			stroke(0);
+			push();
+			translate(x*scl, y*scl);
+			rotate(v.heading());
+			line(0, 0, scl, 0);
+			pop();
+			
 		}
 		yoff += 0.01;
+		zoff += 0.001;
 	}
+
+	fr.html(floor(frameRate()))
 }
